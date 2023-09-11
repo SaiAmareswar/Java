5f55503d5358b658fd733bf22097a7ad6e5f37f6
G Sai Amareswar <91476781+SaiAmareswar@users.noreply.github.com>
2023-09-12 01:39:30 +0530

    1st file
    basics about the data types

---
+++ basics.java
@@ -0,0 +1,22 @@
+// primitive data types
+
+class basics 
+{
+public static void main(String a[])
+    {
+        double f = 6.9;
+        float b = 6.9f;
+        byte c = 8;
+        int x = 3;  // = is assignment property
+        int y = 5;
+        long g = 12345l;
+        byte h = 127;
+        short i = 555;
+        int r = x+y+c;
+        char d = 'e';
+        boolean j = true;
+
+        System.out.println(r); //println means new line
+        System.out.println("hello"); //println means new line
+    }
+}
